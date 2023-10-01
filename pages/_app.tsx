bf48fd26e8b13ef0dd603226e3b987aa2ca64347
devkishor <kishor8007kc@gmail.com>
2023-10-01 23:59:24 +0545

    feat: i18n translation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,13 +3,42 @@ import type { AppProps } from 'next/app'
 import { ChakraProvider } from '@chakra-ui/react'
 import { Provider } from 'react-redux'
 import { store } from '../redux/store'
+import { initReactI18next, I18nextProvider } from "react-i18next";
+import i18n from "i18next";
+
+i18n
+  .use(initReactI18next)
+  .init({
+    resources: {
+      en: {
+        // english
+        translation: require('../locales/en.json')
+      },
+      de: {
+        // germany
+        translation: require('../locales/de.json')
+      },
+      es: {
+        // spanish
+        translation: require('../locales/es.json')
+      }
+    },
+    lng: "en",
+    fallbackLng: "en",
+
+    interpolation: {
+      escapeValue: false 
+    }
+  });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
+    <I18nextProvider i18n={i18n}>
     <ChakraProvider>
       <Component {...pageProps} />
     </ChakraProvider>
+    </I18nextProvider>
     </Provider>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,23 @@
 import { Container, Text, Stack, Heading, Button } from '@chakra-ui/react'
 import Link from 'next/link'
+import { useTranslation } from "react-i18next";
+import {
+  Menu,
+  MenuButton,
+  MenuList,
+  MenuItem,
+  MenuItemOption,
+  MenuGroup,
+  MenuOptionGroup,
+  MenuDivider,
+} from '@chakra-ui/react'
 
 export default function Home() {
+  const { t, i18n } = useTranslation();
+
+  const changeLanguage = (lng: any) => {
+    i18n.changeLanguage(lng);
+  };
 
   return (
     <Container maxW={'5xl'}>
@@ -11,20 +27,30 @@ export default function Home() {
         spacing={{ base: 8, md: 10 }}
         py={{ base: 20, md: 28 }}
       >
+        <Menu>
+          <MenuButton as={Button}>
+            Change Language
+          </MenuButton>
+          <MenuList>
+            <MenuItem>Germany</MenuItem>
+            <MenuItem>English</MenuItem>
+            <MenuItem>Spanish</MenuItem>
+          </MenuList>
+        </Menu>
         <Heading
           fontWeight={500}
           fontSize={{ base: '3xl', sm: '4xl', md: '6xl' }}
-        >Exploring the Uncharted Wilderness</Heading>
-        <Text>Embark on an adventurous journey through unexplored landscapes, where every step unveils the beauty of nature and the mysteries of the unknown. Immerse yourself in the tranquility of pristine forests, discover hidden waterfalls, and encounter fascinating wildlife. This is an expedition for those who seek the thrill of the untamed and the wonder of the undiscovered.</Text>
+        >{t('e-title')}</Heading>
+        <Text>{t('e-description')}</Text>
 
         <Stack spacing={6} direction={'row'}>
           <Button
             rounded={'full'}
             colorScheme={'orange'}
             bg={'orange.400'}
-          >Get Started</Button>
+          >{t('get-started')}</Button>
           <Link href={'/contact'}>
-          <Button rounded={'full'}>Contact Us</Button>
+            <Button rounded={'full'}>{t('contact-us')}</Button>
           </Link>
         </Stack>
       </Stack>
